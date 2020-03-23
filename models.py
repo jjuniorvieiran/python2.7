730323b06321e1084a8f5b06f44a0b8f73545471
Junior <junior.jaava@gmail.com>
2020-03-23 10:39:43 -0300

    working with staticmethod and classmethod

--- models.py
+++ models.py
@@ -1,27 +1,48 @@
 # -*- coding: UTF-8 -*-
 class Perfil(object):
     'Classe padrão para perfis de usuários'
+
     def __init__(self, nome, telefone, empresa):
         self.nome = nome
         self.telefone = telefone
         self.empresa = empresa
         self.__curtidas = 0
-    
+
     def imprimir(self):
-        print "Nome : %s, Telefone: %s, Empresa %s" % (self.nome, self.telefone, self.empresa)  
+        print "Nome : %s, Telefone: %s, Empresa %s" % (
+            self.nome, self.telefone, self.empresa)
 
     def curtir(self):
-        self.__curtidas+=1  
+        self.__curtidas += 1
 
     def obter_curtidas(self):
         return self.__curtidas
 
+    # @staticmethod
+    # def gerar_perfis(nome_arquivo):
+    #     arquivo = open(nome_arquivo,'r')
+    #     perfis = []
+    #     for linha in arquivo:
+    #         valores = linha.split(',')
+    #         perfis.append(Perfil(*valores))
+    #     arquivo.close()
+    #     return perfis
+
+    @classmethod
+    def gerar_perfis(classe, nome_arquivo):
+       arquivo = open(nome_arquivo,'r')
+       perfis = []
+       for linha in arquivo:
+            valores = linha.split(',')
+            perfis.append(classe(*valores))
+       arquivo.close()
+       return perfis
 
 # -*- coding: UTF-8 -*-
 class Perfil_Vip(Perfil):
     'Classe padrão para perfis de usuários VIPs'
 
-    def __init__(self, nome, telefone, empresa, apelido):
+    def __init__(self, nome, telefone, empresa, apelido=''):
         super(Perfil_Vip, self).__init__(nome, telefone, empresa)
         self.apelido = apelido
 
